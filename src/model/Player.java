a94e2f03a5a4d0ed20c7f590ea73516986cefdb3
thitirat1 <thitirat@code-app.com>
2019-10-24 22:09:08 +0700

    added skeleton method to class

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,7 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Player {
     ArrayList<Utility> utilities;
     ArrayList<Estate> estates;
     double money;
+
+    Player(double startingMoney) {
+        money = startingMoney;
+        utilities = new ArrayList<Utility>();
+        estates = new ArayList<Estate>();
+    }
+
+    public void buy(Property property) {}
+    public void drawCard(ArrayList<Card> cards) {};
+    public int rollDice() {};
+    public double pay() {};
+    public void getMoney(double money) {};
+    public void mortgage(Property property) {};
 }
