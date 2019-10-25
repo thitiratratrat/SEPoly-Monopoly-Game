5c31c7b607e55d7653e6f84a618ccefbc9f20d67
Thitiratratrat <altothiti@gmail.com>
2019-10-26 01:07:32 +0700

    create class estate space

--- src/model/EstateSpace.java
+++ src/model/EstateSpace.java
@@ -1,6 +1,57 @@
 package model;
 
-public class EstateSpace extends Space implements Property {
-    private double price;
-    private Player owner;
+import java.util.ArrayList;
+
+public class EstateSpace extends PropertySpace {
+    private double housePrice;
+    private double landmarkPrice;
+    private int houseCount;
+    private int landmarkCount;
+    private ArrayList<Double> rentPrices;
+
+    EstateSpace(int number, String name, double price, double rent, double oneHouseRent,
+                double twoHouseRent, double threeHouseRent, double fourHouseRent,
+                double landmarkRent, double housePrice, double landmarkPrice) {
+        super(number, name, price);
+        this.housePrice = housePrice;
+        this.landmarkPrice = landmarkPrice;
+        houseCount = 0;
+        landmarkCount = 0;
+        rentPrices = new ArrayList<Double>();
+
+        rentPrices.add(rent);
+        rentPrices.add(oneHouseRent);
+        rentPrices.add(twoHouseRent);
+        rentPrices.add(threeHouseRent);
+        rentPrices.add(fourHouseRent);
+        rentPrices.add(landmarkRent);
+    }
+
+    public double getHousePrice() {
+        return housePrice;
+    }
+
+    public double getLandmarkPrice() {
+        return landmarkPrice;
+    }
+
+    public int getHouseCount() {
+        return houseCount;
+    }
+
+    public int getLandmarkCount() {
+        return landmarkCount;
+    }
+
+    public double getRentPrice() {
+        return rentPrices.get(houseCount);
+    }
+
+    public void sellHouse(int sellHouseCount) {
+        houseCount -= sellHouseCount;
+    }
+
+    public void sellLandmark() {
+        landmarkCount = 0;
+    }
 }
