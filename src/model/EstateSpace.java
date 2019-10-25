42043b9ff77e29f60dc6134e35f14cbf86e025bd
thitirat1 <thitirat@code-app.com>
2019-10-25 23:22:25 +0700

    refactor class UtilitySpace and created PropertySpace abstract class

---
+++ src/model/EstateSpace.java
@@ -0,0 +1,6 @@
+package model;
+
+public class EstateSpace extends Space implements Property {
+    private double price;
+    private Player owner;
+}
---
+++ src/model/PropertySpace.java
@@ -0,0 +1,29 @@
+package model;
+
+public abstract class PropertySpace extends Space implements Property {
+    protected double price;
+    protected Player owner;
+
+    PropertySpace(int number, String name, double price) {
+        super(number, name);
+        action = "property";
+        this.price = price;
+        owner = null;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void soldTo(Player player) {
+        owner = player;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+}
