8d25d835b5f36ffb15923f06df05e13c8f32b894
candyccyn <61090004@kmitl.ac.th>
2019-11-08 19:49:25 +0700

    Players walk in the board

---
+++ src/allSpritePlayer/SpriteSheet.java
@@ -0,0 +1,15 @@
+package allSpritePlayer;
+
+import java.awt.image.BufferedImage;
+
+public class SpriteSheet {
+    public BufferedImage spriteSheet;
+    public SpriteSheet(BufferedImage ss){
+        this.spriteSheet = ss;
+    }
+
+    public BufferedImage grabSprite(int x , int y , int width, int height) {
+        BufferedImage sprite = spriteSheet.getSubimage(x,y,width,height);
+        return sprite;
+    }
+}
