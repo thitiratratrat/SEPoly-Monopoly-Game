2d65817b847df85a3529ed17247ea50ea88e7da1
candyccyn <61090004@kmitl.ac.th>
2019-11-13 03:51:32 +0700

    animate Dice

---
+++ src/DiceAnimate/Animator.java
@@ -0,0 +1,54 @@
+package DiceAnimate;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+public class Animator {
+    ArrayList<BufferedImage> frames ;
+    BufferedImage sprite;
+    private volatile boolean running =false;
+    private long previosTime,speed ;
+    private int frameAtPause ,currentFrame;
+    public Animator(ArrayList<BufferedImage> frames){
+        this.frames = frames;
+    }
+    public void setSpeed(long speed){
+        this.speed = speed;
+    }
+
+    public void update(long time){
+        if (running){
+            if(time - previosTime >= speed){
+                currentFrame++;
+                try{
+                    sprite = frames.get(currentFrame);
+                }catch (IndexOutOfBoundsException e){
+                    currentFrame = 0;
+                    sprite = frames.get(currentFrame);
+                }
+                previosTime = time;
+            }
+        }
+    }
+
+    public void start(){
+        running = true;
+        previosTime = 0;
+        frameAtPause = 0;
+        currentFrame = 0 ;
+    }
+
+    public void stop( ){
+        running = false;
+        previosTime = 0;
+        frameAtPause = 0;
+        currentFrame = 0 ;
+    }
+    public void pause( ){
+        frameAtPause = currentFrame ;
+        running = false;
+    }
+    public void resume( ){
+        currentFrame = frameAtPause;
+        running =true;
+    }
+}
---
+++ src/DiceAnimate/Main.java
@@ -0,0 +1,126 @@
+package DiceAnimate ;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.Random;
+
+public class Main extends JFrame {
+    BufferedImage spriteDice;
+    Animator rollDice ,rollDice2;
+    boolean ckA=true;
+
+    int die1 = 0, die2 =0 ;
+
+    public Main(){
+        setSize(800,600);
+        setVisible(true);
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        init();
+    }
+
+    private void init() {
+        BufferedImageLoader loader = new BufferedImageLoader() ;
+        BufferedImage diceSpriteSheet= null ;
+        try {
+            diceSpriteSheet = loader.loadImage("C:\\Users\\us\\Desktop\\Y2S1\\JAVA\\monopoly\\src\\allImage\\newDice.png" );
+        }catch (IOException ex){
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,ex);
+        }
+        SpriteSheet diceSS = new SpriteSheet(diceSpriteSheet);
+        SpriteSheet diceSS2 = new SpriteSheet(diceSpriteSheet);
+
+        ArrayList<BufferedImage> sprites =new ArrayList<BufferedImage>();
+        ArrayList<BufferedImage> sprites2 =new ArrayList<BufferedImage>();
+
+
+        Random random = new Random();
+        die1  = random.nextInt(6) + 1;
+        die2  = random.nextInt(6) + 1;
+
+
+        sprites.add(diceSS.grabSprite(0, 0, 92,92));//1
+        sprites.add(diceSS.grabSprite(92, 0, 92,92));//2
+        sprites.add(diceSS.grabSprite(184, 0, 92,92));//3
+        sprites.add(diceSS.grabSprite(276, 0, 92,92));
+        sprites.add(diceSS.grabSprite(0, 92, 92,92));
+        sprites.add(diceSS.grabSprite(92, 92, 92,92));
+        switch (die1) {
+            case 1: sprites.add(diceSS.grabSprite(184, 92, 92,92));break;
+            case 2: sprites.add(diceSS.grabSprite(276, 92, 92,92));break;
+            case 3: sprites.add(diceSS.grabSprite(0, 184, 92,92));break;
+            case 4: sprites.add(diceSS.grabSprite(92, 184, 92,92));break;
+            case 5: sprites.add(diceSS.grabSprite(184, 184, 92,92));break;
+            case 6: sprites.add(diceSS.grabSprite(276, 184, 92,92));break;
+        }
+
+        sprites2.add(diceSS2.grabSprite(0, 0, 92,92));//1
+        sprites2.add(diceSS2.grabSprite(92, 0, 92,92));//2
+        sprites2.add(diceSS2.grabSprite(184, 0, 92,92));//3
+        sprites2.add(diceSS2.grabSprite(276, 0, 92,92));
+        sprites2.add(diceSS2.grabSprite(0, 92, 92,92));
+        sprites2.add(diceSS2.grabSprite(92, 92, 92,92));
+        switch (die2) {
+            case 1: sprites2.add(diceSS2.grabSprite(184, 92, 92,92));break;
+            case 2: sprites2.add(diceSS2.grabSprite(276, 92, 92,92));break;
+            case 3: sprites2.add(diceSS2.grabSprite(0, 184, 92,92));break;
+            case 4: sprites2.add(diceSS2.grabSprite(92, 184, 92,92));break;
+            case 5: sprites2.add(diceSS2.grabSprite(184, 184, 92,92));break;
+            case 6: sprites2.add(diceSS2.grabSprite(276, 184, 92,92));break;
+        }
+
+        rollDice = new Animator(sprites);
+        rollDice.setSpeed(100);
+        rollDice.start();
+
+        rollDice2 = new Animator(sprites2);
+        rollDice2.setSpeed(100);
+        rollDice2.start();
+
+        try
+        {
+            Thread.sleep(2000);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+        rollDice.pause();
+        rollDice2.pause();
+    }
+
+    Image dbImage;
+    Graphics dbg ;
+    public void paint(Graphics g) {
+        dbImage = createImage(getWidth(),getHeight());
+        dbg = dbImage.getGraphics();
+        paintComponents(dbg);
+        g.drawImage(dbImage,0,0,null);
+    }
+
+    public void paintComponents(Graphics g ){
+        if(rollDice != null ){
+            rollDice.update(System.currentTimeMillis());
+            g.drawImage(rollDice.sprite,380,460,50,50,null);
+
+        }
+        if(rollDice2 != null ){
+            rollDice2.update(System.currentTimeMillis());
+            g.drawImage(rollDice2.sprite,460,460,50,50,null);
+        }
+        repaint();
+    }
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        // write your code here
+    }
+}
